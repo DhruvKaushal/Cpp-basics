4419e1865dad4c5a5f58997907400f863cec8de4
DhruvKaushal <dhruvkaushal1997@gmail.com>
2018-10-05 10:07:08 +0530

    Array basics
    Signed-off-by: DhruvKaushal <dhruvkaushal1997@gmail.com>

---
+++ Arrays/reverseArray.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  long n;
+  cin>>n;
+  long a[n];
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  for(int i=n-1;i>=0;i--){
+    cout<<a[i]<<endl;
+  }
+  return 0;
+}
