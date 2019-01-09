786f3b90c9eb46cacb9d4777755c8a8177bd3a82
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-10 00:19:08 +0530

    rotate 2d array without extra space

--- q2.cpp
+++ q2.cpp
@@ -1,14 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int t;
+
+int main() {
+	int t;
   cin>>t;
   while(t){
-    string a,b;
-    int x,y;
-    cin>>x>>y;
-
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        cin>>a[i][j];
+      }
+    }
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        cout<<a[n-j-1][i]<<" ";
+      }
+    }
     t--;
   }
-  return 0;
+	return 0;
 }
