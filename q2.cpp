b383ac307daf3c7b483522c083bfc022a6065f83
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-10 19:02:37 +0530

    Next biggest element of array

--- q2.cpp
+++ q2.cpp
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int larg(int x[], int y[], int z){
+	int j=0;
+	while(x[j]<=y[z]){
+		j++;
+	}
+	return x[j];
+}
 int main() {
 	int t;
   cin>>t;
   while(t){
     int n;
     cin>>n;
-    int a[n][n];
-    for(int i=0;i<n;i++){
-      for(int j=0;j<n;j++){
-        cin>>a[i][j];
-      }
-    }
-    for(int i=0;i<n;i++){
-      for(int j=0;j<n;j++){
-        cout<<a[n-j-1][i]<<" ";
-      }
-    }
+    int a[n], b[n];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+			b[i] = a[i];
+		}
+		sort(b,b+n);
+		int maxi = b[n-1];
+		for(int i=0;i<n;i++){
+			if(a[i]==maxi){
+				cout<<"-1"<<" ";
+			}
+			else if (a[i]!=maxi){
+				int next = larg(b, a, i);
+				cout<<next<<" ";
+			}
+		}
     t--;
   }
 	return 0;
