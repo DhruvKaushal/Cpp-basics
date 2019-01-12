e97c0f2952fe6ea05d11ca8c88ee70c3783c14ea
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-12 21:02:47 +0530

    no of squares in chess board

--- q2.cpp
+++ q2.cpp
@@ -1,40 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
 	int t;
   cin>>t;
   while(t){
-    int n;
-    cin>>n;
-    int a[n];
-		for(int i=0;i<n;i++){
-			cin>>a[i];
+    int n,nos=0;
+		cin>>n;
+		for(int i=1;i<=n;i++){
+			nos+=i*i;
 		}
-		int low=0, mid=0, end=n-1;
-		while(mid<=end){
-			if(a[mid]==0){
-				int temp=a[low];
-				a[low]=a[mid];
-				a[mid]=temp;
-				mid++;
-				low++;
-			}
-
-			else if(a[mid]==1){
-				mid++;
-			}
-			else if(a[mid]==2){
-				int temp=a[end];
-				a[end]=a[mid];
-				a[mid]=temp;
-				end--;
-			}
-		}
-		for(int i=0;i<n;i++){
-			cout<<a[i]<<" ";
-		}
-		cout<<endl;
+		cout<<nos<<endl;
     t--;
   }
 	return 0;
