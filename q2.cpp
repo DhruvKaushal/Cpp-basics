193e58ed91a0bc5a0147b131e96276768ea4eacf
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-14 21:03:16 +0530

    Fibonacci series

--- q2.cpp
+++ q2.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int t;
-  cin>>t;
-  while(t){
-    int n,nos=0;
+	long t;
+	cin>>t;
+	while(t){
+		long n;
 		cin>>n;
-		for(int i=1;i<=n;i++){
-			nos+=i*i;
+		long first = 1, second =1;
+		if(n==1){
+			cout<<"1";
+			return 0;
 		}
-		cout<<nos<<endl;
-    t--;
-  }
+		else{
+			cout<<first<<" "<<second<<" ";
+			for(long i=0;i<n-2;i++){
+				long long third=first+second;
+				cout<<third<<" ";
+				first = second;
+				second = third;
+			}
+		}
+		t--;
+	}
+
 	return 0;
 }
