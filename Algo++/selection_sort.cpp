1b6d96db085b2c4cbb9d9daf23d66e4bff89aee3
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-16 21:03:49 +0530

    Selection sort

---
+++ Algo++/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int size, i, j, temp;
+	cin>>size;
+  int arr[size];
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Sorting array using selection sort...\n";
+	for(i=0; i<size; i++)
+	{
+		for(j=i+1; j<size; j++)
+		{
+			if(arr[i]>arr[j])
+			{
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+	cout<<"Now the Array after sorting is :\n";
+	for(i=0; i<size; i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
