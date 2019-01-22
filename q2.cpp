9ed2e58301c5330468964b4f4b4b079d5ded1570
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-22 21:39:44 +0530

    immediate smaller element

--- q2.cpp
+++ q2.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int tilesc(int n, int m){
-	if(n==1){
-		return 1;
-	}
-	if(m==1){
-		return n;
-	}
-	if(n==m){
-		return n;
-	}
-	return tilesc(n-1,m) + tilesc(n-m,m);
-}
 int main() {
 	int t;
 	cin>>t;
 	while(t){
-		int n,m,count;
-		cin>>n>>m;
-		count = tilesc(n,m);
-		cout<<count<<endl;
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		for(int i=0;i<n-1;i++){
+			if(a[i+1]<a[i]){
+				cout<<a[i+1]<<" ";
+			}
+			else{
+				cout<<"-1"<<" ";
+			}
+		}
+		cout<<"-1"<<endl;
 		t--;
 	}
 	return 0;
