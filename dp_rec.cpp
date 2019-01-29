992fdc0a6af4ddc2a4bcf4221959ae959e30961d
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-01-29 21:49:51 +0530

    no of ways using dp

--- dp_rec.cpp
+++ dp_rec.cpp
@@ -1,11 +1,31 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int calcR(int n, int m){
-  if(n==1 || m==1){
-    return 1;
-  }
+// int calcR(int n, int m){              //using recursion
+//   if(n==1 || m==1){
+//     return 1;
+//   }
+//   return calcR(n-1,m) + calcR(n,m-1);
+// }
 
-  return calcR(n-1,m) + calcR(n,m-1);
+int calcR(int n, int m){
+  int a[n][m];
+  for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
+      a[i][j] = -1;
+    }
+  }
+  for(int i=0;i<n;i++){
+    a[i][0]=1;
+  }
+  for(int i=0;i<m;i++){
+    a[0][i]=1;
+  }
+  for(int i=1;i<n;i++){
+    for(int j=1;j<m;j++){
+      a[i][j] = a[i-1][j] + a[i][j-1];
+    }
+  }
+  return a[n-1][m-1];
 }
 int main(){
   int t;
