5f2ab6847429c4c5c42a65a60414287f33c7c8e5
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-02-06 13:47:05 +0530

    count no of 1's in binary rep

--- hack.cpp
+++ hack.cpp
@@ -1,15 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  string s;
-  cin>>s;
-  int st=0,en=s.length()-1;
-  int x = calcR(s,st,en);
-  if(x==0){
-    cout<<"false";
-  }
-  else{
-    cout<<"true";
+
+  int t;
+  cin>>t;
+  while(t){
+    int l,r;
+    cin>>l>>r;
+    int count=0;
+    for(int i=l;i<=r;i++){
+      int x=i;
+      if(i==0){
+        continue;
+      }
+      else if(i<0){
+        x=(~x)+1;
+        while(x>0){
+          if((x&1)==1){
+            count++;
+          }
+          x=(x>>1);
+        }
+        count=32-count;
+      }
+      else if(i>0){
+        while(x>0){
+          if((x&1)==1){
+            count++;
+          }
+          x=(x>>1);
+        }
+      }
+
+    }
+    cout<<count<<endl;
+    t--;
   }
   return 0;
 }
