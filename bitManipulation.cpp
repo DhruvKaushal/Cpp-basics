6b9f660961ecdc45e47f554e567f078fd03ddeb4
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-03-19 19:07:48 +0530

    max xor in range

--- bitManipulation.cpp
+++ bitManipulation.cpp
@@ -1,16 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int n;
-  cin>>n;
-  int a[n];
-  int x=0;
-  for(int i=0;i<n;i++){
-    cin>>a[i];
-  }
-  for(int i=0;i<n;i++){
-    x=(x^a[i]);
-  }
-  cout<<x;
-  return 0;
+
+// method to get maximum xor value in range [L, R]
+int maxXORInRange(int L, int R)
+{
+    // get xor of limits
+    int LXR = L ^ R;
+
+    //  loop to get msb position of L^R
+    int msbPos = 0;
+    while (LXR)
+    {
+        msbPos++;
+        LXR >>= 1;
+    }
+
+    // construct result by adding 1,
+    // msbPos times
+    int maxXOR = 0;
+    int two = 1;
+    while (msbPos--)
+    {
+        maxXOR += two;
+        two <<= 1;
+    }
+
+    return maxXOR;
+}
+
+//  Driver code to test above methods
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout << maxXORInRange(a,b) << endl;
+    return 0;
 }
