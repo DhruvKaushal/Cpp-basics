0f28c89201ab3ac8fe27091a6752022ca2d9a1bd
DhruvKaushal <dhruvkaushal1997@gmail.com>
2019-03-24 18:57:42 +0530

    longest consec subseq

--- gfgDP.cpp
+++ gfgDP.cpp
@@ -1,24 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+int findLongestConseqSubseq(int [], int );
+int main() {
+	int t;
+	cin>>t;
 
-int main(){
-  int t;
-  cin>>t;
-  while(t){
-    int n,m,mod=pow(10,9)+7;
-    cin>>n>>m;
-    int a[n][m];
-    for(int i=0;i<n;i++){
-      a[0][i]=1;
-      a[i][0]=1;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int a[n];
+
+		// input array elements
+		for(int i=0;i<n;i++)
+		    cin>>a[i];
+
+		cout<< findLongestConseqSubseq(a, n)<<endl;
+
+	}
+	return 0;
+}
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+
+// function to find longest consecutive subsequence
+// n : size of array
+// arr[] : input array
+int findLongestConseqSubseq(int arr[], int n)
+{
+  int b[n];
+  b[0] = 1;
+  sort(arr,arr+n);
+  for(int i=1;i<n;i++){
+    if(arr[i]-arr[i-1]==1){
+      b[i] = b[i-1]+1;
     }
-    for(int i=1;i<n;i++){
-      for(int j=1;j<m;j++){
-        a[i][j] = (a[i-1][j] + a[i][j-1])%mod;
-      }
+    else if(arr[i]==arr[i-1]){
+      b[i]=b[i-1];
     }
-    cout<<a[n-1][m-1]<<endl;
-    t--;
+    else{
+      b[i]=1;
+    }
   }
-  return 0;
+  int m = 0;
+  for(int i=0;i<n;i++){
+    if(b[i]>m){
+      m=b[i];
+    }
+  }
+  return m;
+    //Your code here
 }
